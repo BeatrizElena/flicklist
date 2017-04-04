a80ff458d1cb9b7218968ec26bc9df4ccdb444fa
BeatrizOrtiz <beortiz2010@yahoo.com>
2017-04-04 13:47:14 -0400

    finish studio 4

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
@@ -28,7 +28,7 @@ var api = {
 
 // TODO 1
 // this function should accept a second argument, `keywords`
-function discoverMovies(callback) {
+function discoverMovies(callback, keywords) {
 
   // TODO 2 
   // ask the API for movies related to the keywords that were passed in above
@@ -38,6 +38,8 @@ function discoverMovies(callback) {
     url: api.root + "/discover/movie",
     data: {
       api_key: api.token,
+      with_keywords: keywords,
+      method: "GET",
     },
     success: function(response) {
       model.browseItems = response.results;
@@ -91,18 +93,32 @@ function searchMovies(query, callback) {
 
 
   $.ajax({
-    url: api.root + "/search/movie",
+    url: api.root + "/search/keyword",
     data: {
       api_key: api.token,
       query: query
     },
     success: function(response) {
       console.log(response);
-    }
+
+    var keywordIDs = response.results.map(idObject);
+		console.log(keywordIDs);
+	var keywordsString = keywordIDs.join('|');
+		console.log(keywordsString);
+	
+	discoverMovies(callback, keywordsString);
+	}
   });
 }
+function idObject(query){
+	return query.id;
+}
 
-
+// var keywordIDs = response.results.map(response.id)
+// console.log(keywordIDs);
+// 
+// var keywordsString = keywordIDs.join('|')
+// console.log(keywordsString);
 /**
  * re-renders the page with new content, based on the current state of the model
  */
